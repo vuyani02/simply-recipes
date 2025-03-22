83213ae3360d9e7956e87a6f7fb3d0a340da10fb
mtsvuy022 <146190337+vuyani02@users.noreply.github.com>
2025-03-22 02:57:10 +0200

    completed the home page and made it responsive

--- js/index.js
+++ js/index.js
@@ -7,10 +7,12 @@ throw Error(
 }
 
 const links = getElement('.nav-links')
+const container = getElement('.container')
 const navBtnDOM = getElement('.nav-btn')
 
 navBtnDOM.addEventListener('click', () => {
-links.classList.toggle('show-links')
+links.classList.toggle('show-links') 
+container.classList.toggle('increase-h') 
 })
 
 const date = getElement('#date')
