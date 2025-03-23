284bcdd8f52e934b0d3ded1d08ba7644f7fdc38c
mtsvuy022 <146190337+vuyani02@users.noreply.github.com>
2025-03-23 15:26:40 +0200

    finished designing the 404, recipes and tag template pages

--- js/newjs.js
+++ js/newjs.js
@@ -7,12 +7,12 @@ throw Error(
 }
 
 const links = getElement('.nav-links')
-const container = getElement('.container')
+const container = getElement('.container-new')
 const navBtnDOM = getElement('.nav-btn')
 
 navBtnDOM.addEventListener('click', () => {
 links.classList.toggle('show-links') 
-container.classList.toggle('increase-h') 
+container.classList.toggle('increase-h-new') 
 })
 
 const date = getElement('#yr')
